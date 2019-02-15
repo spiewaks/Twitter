619a572ce45ac0a6a1b0cc819cf9fcd3311ec4da
spiewaks <sylwia.spiewak123@gmail.com>
2019-02-15 12:34:04 +0100

    add Database file connection

--- src/DatabaseConnection.php
+++ src/DatabaseConnection.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: carierchoice
- * Date: 15.02.19
- * Time: 12:20
- */
+require_once('Config.php');
+global $conn;
+$conn = null;
+try {
+    $conn = new PDO(sprintf('mysql:host=%s;dbname=%s;charset=utf8', DATABASE_HOST, DATABASE_NAME),
+        DATABASE_USER_NAME,
+        DATABASE_USER_PASSWORD,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+} catch (PDOException $e) {
+    die('Wystąpił błąd połączenia do bazy danych');
+}
