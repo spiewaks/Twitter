83d7ac4821a6c369297370c34aa6a87ba6badafd
spiewaks <sylwia.spiewak123@gmail.com>
2019-02-15 12:33:18 +0100

    Add Config.php

---
+++ src/Config.php
@@ -0,0 +1,5 @@
+<?php
+define('DATABASE_USER_NAME', 'sksz');
+define('DATABASE_USER_PASSWORD', '1qwasa');
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_NAME', 'twitter');
---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carierchoice
+ * Date: 15.02.19
+ * Time: 12:20
+ */
